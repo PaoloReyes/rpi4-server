38f1947388cf0081c335d1728566567e1c6c6511
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-07-13 15:36:52 -0600

    handle intial internet access for electricity cuts

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
-#Server imports
+# Server imports
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-#Subscripts imports
+# Subscripts imports
 import sys
 sys.path.insert(1, '/home/paolo/ssh')
 sys.path.insert(1, '/home/paolo/stock-portfolio/python')
 from ngrok_tunnels import Ngrok_Connection
 from stock_portfolio import Stock_Portfolio
 
-#App
+# App
 app = FastAPI()
 
-#CORS
+# CORS
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=["*"],
@@ -20,11 +20,18 @@ app.add_middleware(
 	allow_headers=["*"],
 )
 
-#Subscripts
+# Subscripts
 portfolio = Stock_Portfolio()
-ngrok_connection = Ngrok_Connection('web-server', 'ssh')
 
-#Stock Portfolio Stuff
+internet_access = False
+while not internet_access:
+	try:
+		ngrok_connection = Ngrok_Connection('web-server', 'ssh')
+		internet_access = True
+	except:
+		internet_access = False
+
+# Stock Portfolio Stuff
 @app.get('/stock-portfolio/register')
 def stock_portfolio_register_data(hey: float = 0.0, bbva: float = 0.0, nu: float = 0.0, up_sivale: float = 0.0, fibras: float = 0.0, etfs: float = 0.0, cetes: float = 0.0, savings_fund: float = 0.0, finsus: float = 0.0, mercado_pago: float = 0.0, bitso: float = 0.0, cash: float = 0.0):
     return portfolio.register_data(hey, bbva, nu, up_sivale, fibras, etfs, cetes, savings_fund, finsus, mercado_pago, bitso, cash)
@@ -49,7 +56,7 @@ def stock_portfolio_register_prediction(rate: float = 0.0, years: int = 0, contr
 def stock_portfolio_request_prediction():
 	return portfolio.request_prediction()
 
-#Ngrok Stuff
+# Ngrok Stuff
 @app.get('/ngrok/ssh')
 def get_ngrok_process():
 	return ngrok_connection.get_ngrok_tcp(22)
